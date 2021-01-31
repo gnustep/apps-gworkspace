5bbeb920f35b41badbb5fb24fbfa047a83a52c90
Riccardo Mottola <rm@gnu.org>
2021-01-31 21:36:41 +0100

    Cleanup code and fix display of extensions without a valid icons.

--- Inspector/ContentViewers/AppViewer/AppViewer.m
+++ Inspector/ContentViewers/AppViewer/AppViewer.m
@@ -1,6 +1,6 @@
 /* AppViewer.m
  *  
- * Copyright (C) 2004-2018 Free Software Foundation, Inc.
+ * Copyright (C) 2004-2021 Free Software Foundation, Inc.
  *
  * Authors: Enrico Sersale <enrico@imago.ro>
  *          Riccardo Mottola <rm@gnu.org>
@@ -30,8 +30,8 @@
 
 - (void)dealloc
 {
-	RELEASE (scroll);
-	RELEASE (explField);
+  RELEASE (scroll);
+  RELEASE (explField);
   RELEASE (errLabel);
   TEST_RELEASE (currentPath);
   [super dealloc];
@@ -40,85 +40,86 @@
 - (id)initWithFrame:(NSRect)frameRect
           inspector:(id)insp
 {
-	self = [super initWithFrame: frameRect];
+  self = [super initWithFrame: frameRect];
 	
-	if (self) {	
-    NSRect r, vr;
-    float x, y, w, h;
-		id cell;
+  if (self)
+    {	
+      NSRect r, vr;
+      CGFloat x, y, w, h;
+      NSButtonCell *cell;
     
-    r = [self bounds];
+      r = [self bounds];
     
-    x = 5;
-    y = r.origin.y + 182;
-    w = r.size.width - 10;
-    h = 20;
-    vr = NSMakeRect(x, y, w, h);
-  	explField = [[NSTextField alloc] init];	
-		[explField setFrame: vr];
-  	[explField setAlignment: NSCenterTextAlignment];
-		[explField setFont: [NSFont systemFontOfSize: 12]];
-  	[explField setBackgroundColor: [NSColor windowBackgroundColor]];
-  	[explField setTextColor: [NSColor darkGrayColor]];	
-  	[explField setBezeled: NO];
-  	[explField setEditable: NO];
-  	[explField setSelectable: NO];
-		[explField setStringValue: NSLocalizedString(@"Open these kinds of documents:", @"")];
-  	[self addSubview: explField];
+      x = 5;
+      y = r.origin.y + 182;
+      w = r.size.width - 10;
+      h = 20;
+      vr = NSMakeRect(x, y, w, h);
+      explField = [[NSTextField alloc] init];	
+      [explField setFrame: vr];
+      [explField setAlignment: NSCenterTextAlignment];
+      [explField setFont: [NSFont systemFontOfSize: 12]];
+      [explField setBackgroundColor: [NSColor windowBackgroundColor]];
+      [explField setTextColor: [NSColor darkGrayColor]];	
+      [explField setBezeled: NO];
+      [explField setEditable: NO];
+      [explField setSelectable: NO];
+      [explField setStringValue: NSLocalizedString(@"Open these kinds of documents:", @"")];
+      [self addSubview: explField];
 		
-    w = 196;    
-    h = 94;
-    x = (r.size.width - w) / 2;
-    y = r.origin.y + 85;
-    vr = NSMakeRect(x, y, w, h);    
-    scroll = [[NSScrollView alloc] initWithFrame: vr];
-    [scroll setBorderType: NSBezelBorder];
-    [scroll setHasHorizontalScroller: YES];
-    [scroll setHasVerticalScroller: NO]; 
-    [self addSubview: scroll]; 
+      w = 196;    
+      h = 94;
+      x = (r.size.width - w) / 2;
+      y = r.origin.y + 85;
+      vr = NSMakeRect(x, y, w, h);    
+      scroll = [[NSScrollView alloc] initWithFrame: vr];
+      [scroll setBorderType: NSBezelBorder];
+      [scroll setHasHorizontalScroller: YES];
+      [scroll setHasVerticalScroller: NO]; 
+      [self addSubview: scroll]; 
 		
-    cell = [NSButtonCell new];
-    [cell setButtonType: NSPushOnPushOffButton];
-    [cell setImagePosition: NSImageAbove]; 
+      cell = [NSButtonCell new];
+      [cell setButtonType: NSPushOnPushOffButton];
+      [cell setImagePosition: NSImageAbove]; 
 				
-    matrix = [[NSMatrix alloc] initWithFrame: NSZeroRect
-				            				mode: NSRadioModeMatrix prototype: cell
-			       												numberOfRows: 0 numberOfColumns: 0];
-    RELEASE (cell);
-    [matrix setIntercellSpacing: NSZeroSize];
-    h = [[scroll contentView] bounds].size.height;
-    [matrix setCellSize: NSMakeSize(64, h)];
-		[matrix setAllowsEmptySelection: YES];
-		[scroll setDocumentView: matrix];	
-    RELEASE (matrix);
+      matrix = [[NSMatrix alloc] initWithFrame: NSZeroRect
+					  mode: NSRadioModeMatrix prototype: cell
+				  numberOfRows: 0 numberOfColumns: 0];
+      RELEASE (cell);
+      [matrix setIntercellSpacing: NSZeroSize];
+      h = [[scroll contentView] bounds].size.height;
+      [matrix setCellSize: NSMakeSize(64, h)];
+      [matrix setAllowsEmptySelection: YES];
+      [scroll setDocumentView: matrix];	
+      RELEASE (matrix);
 
-    x = 5;
-    y = r.origin.y + 162;
-    w = r.size.width - 10;
-    h = 25;
-    vr = NSMakeRect(x, y, w, h);
-  	errLabel = [[NSTextField alloc] init];	
-		[errLabel setFrame: vr];
-  	[errLabel setAlignment: NSCenterTextAlignment];
-		[errLabel setFont: [NSFont systemFontOfSize: 18]];
-  	[errLabel setBackgroundColor: [NSColor windowBackgroundColor]];
-  	[errLabel setTextColor: [NSColor darkGrayColor]];	
-  	[errLabel setBezeled: NO];
-  	[errLabel setEditable: NO];
-  	[errLabel setSelectable: NO];
-		[errLabel setStringValue: NSLocalizedString(@"Invalid Contents", @"")];
+      x = 5;
+      y = r.origin.y + 162;
+      w = r.size.width - 10;
+      h = 25;
+      vr = NSMakeRect(x, y, w, h);
+      errLabel = [[NSTextField alloc] init];	
+      [errLabel setFrame: vr];
+      [errLabel setAlignment: NSCenterTextAlignment];
+      [errLabel setFont: [NSFont systemFontOfSize: 18]];
+      [errLabel setBackgroundColor: [NSColor windowBackgroundColor]];
+      [errLabel setTextColor: [NSColor darkGrayColor]];	
+      [errLabel setBezeled: NO];
+      [errLabel setEditable: NO];
+      [errLabel setSelectable: NO];
+      [errLabel setStringValue: NSLocalizedString(@"Invalid Contents", @"")];
 
-    currentPath = nil;
+      currentPath = nil;
 
-    inspector = insp;
-    ws = [NSWorkspace sharedWorkspace];
+      inspector = insp;
+      ws = [NSWorkspace sharedWorkspace];
 				
-		valid = YES;
+      valid = YES;
     
-    [self setContextHelp];
-  }
+      [self setContextHelp];
+    }
 	
-	return self;
+  return self;
 }
 
 - (void)displayPath:(NSString *)path
@@ -129,125 +130,128 @@
   
   ASSIGN (currentPath, path);
 
-  if ([self superview]) {      
+  if ([self superview])      
     [inspector contentsReadyAt: currentPath];
-  }
   
   infok = YES;
   bundle = [NSBundle bundleWithPath: currentPath];
   info = [bundle infoDictionary]; 
-	
-  if (info) {
-    NSFileManager *fm = [NSFileManager defaultManager];
-    id typesAndIcons = [info objectForKey: @"NSTypes"];
-    
-    if (typesAndIcons && [typesAndIcons isKindOfClass: [NSArray class]]) {
-      NSMutableArray *extensions = [NSMutableArray array];  
-      NSMutableDictionary *iconsdict = [NSMutableDictionary dictionary];
-  	  NSString *iname;
-		  id cell;
-		  id typesarr;        
-      int i, j, count;
-    
-      i = [typesAndIcons count];
-      
-      while (i-- > 0) {
-        id entry = [typesAndIcons objectAtIndex: i];
-        
-        if ([entry isKindOfClass: [NSDictionary class]] == NO) {
-					continue;
-				}
-				
-        typesarr = [(NSDictionary *)entry objectForKey: @"NSUnixExtensions"];
 
-        if ([typesarr isKindOfClass: [NSArray class]] == NO) {
-					continue;
-				}
-				
-        j = [typesarr count];
-        iname = [(NSDictionary *)entry objectForKey: @"NSIcon"];
+  if (info)
+    {
+      NSFileManager *fm = [NSFileManager defaultManager];
+      id typesAndIcons = [info objectForKey: @"NSTypes"];
 
-        while (j-- > 0) {
-          NSString *ext = [[typesarr objectAtIndex: j] lowercaseString];
-          [extensions addObject: ext];
-          if (iname != nil) {
-            [iconsdict setObject: iname forKey: ext];
-          }
-        }
-      }
-      
-		  count = [extensions count];
-
-      for (i = 0; i < count; i++) {
-			  NSString *ext1 = [extensions objectAtIndex: i];
-			  NSString *icnname1 = [iconsdict objectForKey: ext1];
-
-    	  for (j = 0; j < count; j++) {
-				  NSString *ext2 = [extensions objectAtIndex: j];
-				  NSString *icnname2 = [iconsdict objectForKey: ext2];
-
-				  if ((i != j) && ([icnname1 isEqual: icnname2])) {
-					  [iconsdict removeObjectForKey: ext1];
-				  }
-			  }
-		  }
-
-		  extensions = [NSMutableArray arrayWithArray: [iconsdict allKeys]];
-		  count = [extensions count];
-
-		  [matrix renewRows: 1 columns: count];
-		  [matrix sizeToCells];
-
-      for (i = 0; i < count; i++)
+      if (typesAndIcons && [typesAndIcons isKindOfClass: [NSArray class]])
 	{
-	  NSString *ext = [extensions objectAtIndex: i];
-	  NSString *icnname;
-	  NSString *iconPath;
-
-	  cell = [matrix cellAtRow: 0 column: i];
-	  [cell setTitle: ext];
-
-	  icnname = [iconsdict objectForKey: ext];
-	  if ([icnname length] > 0)
+	  NSMutableArray *extensions = [NSMutableArray array];  
+	  NSMutableDictionary *iconsdict = [NSMutableDictionary dictionary];
+	  NSString *iname;
+	  id cell;     
+	  NSUInteger i, j, count;
+    
+	  for (i = 0; i < [typesAndIcons count]; i++)
 	    {
-	      iconPath = [bundle pathForImageResource: icnname];
-	      if (iconPath && [fm fileExistsAtPath: iconPath])
+	      id typesarr;
+	      id entry = [typesAndIcons objectAtIndex: i];
+        
+	      if ([entry isKindOfClass: [NSDictionary class]] == NO)
+		continue;
+				
+	      typesarr = [(NSDictionary *)entry objectForKey: @"NSUnixExtensions"];
+
+	      if ([typesarr isKindOfClass: [NSArray class]] == NO)
+		continue;
+				
+	      iname = [(NSDictionary *)entry objectForKey: @"NSIcon"];
+
+	      for (j = 0; j < [typesarr count]; j++)
 		{
-		  NSImage *image = [[NSImage alloc] initWithContentsOfFile: iconPath];
-		  [cell setImage: image];
-		  RELEASE (image);
+		  NSString *ext = [[typesarr objectAtIndex: j] lowercaseString];
+		  if ([extensions indexOfObject:ext] == NSNotFound)
+		    {
+		      [extensions addObject: ext];
+		      if (iname != nil)
+			{
+			  [iconsdict setObject: iname forKey: ext];
+			}
+		    }
 		}
 	    }
-	  else
+
+	  count = [extensions count];
+	  for (i = 0; i < count; i++)
 	    {
-	      [cell setImage: nil]; // reset icon
+	      NSString *ext1 = [extensions objectAtIndex: i];
+	      NSString *icnname1 = [iconsdict objectForKey: ext1];
+
+	      for (j = 0; j < count; j++)
+		{
+		  NSString *ext2 = [extensions objectAtIndex: j];
+		  NSString *icnname2 = [iconsdict objectForKey: ext2];
+
+		  if ((i != j) && ([icnname1 isEqual: icnname2]))
+		    {
+		      [iconsdict removeObjectForKey: ext1];
+		    }
+		}
 	    }
+
+	  [matrix renewRows: 1 columns: count];
+	  [matrix sizeToCells];
+
+	  for (i = 0; i < count; i++)
+	    {
+	      NSString *ext = [extensions objectAtIndex: i];
+	      NSString *icnname;
+	      NSString *iconPath;
+
+	      cell = [matrix cellAtRow: 0 column: i];
+	      [cell setTitle: ext];
+
+	      icnname = [iconsdict objectForKey: ext];
+	      if ([icnname length] > 0)
+		{
+		  iconPath = [bundle pathForImageResource: icnname];
+		  if (iconPath && [fm fileExistsAtPath: iconPath])
+		    {
+		      NSImage *image = [[NSImage alloc] initWithContentsOfFile: iconPath];
+		      [cell setImage: image];
+		      RELEASE (image);
+		    }
+		}
+	      else
+		{
+		  [cell setImage: nil]; // reset icon
+		}
+	    }
+	  [matrix sizeToCells];
+      
+	  if (valid == NO)
+	    {
+	      [errLabel removeFromSuperview]; 
+	      [self addSubview: explField];       
+	      [self addSubview: scroll];       
+	      valid = YES;
+	    }		
 	}
-      [matrix sizeToCells];
-      
-		  if (valid == NO) {
-			  [errLabel removeFromSuperview]; 
-        [self addSubview: explField];       
-        [self addSubview: scroll];       
-			  valid = YES;
-		  }		
-      
-    } else {
-      infok = NO;  
+      else
+	{
+	  infok = NO;  
+	}
     }
-		
-  } else { 
-		infok = NO;  		
-  }
+  else
+    { 
+      infok = NO;  		
+    }
   
-  if (infok == NO) {
-    if (valid == YES) {
-			[explField removeFromSuperview]; 
-			[scroll removeFromSuperview]; 
+  if (infok == NO && valid == YES)
+    {
+      [explField removeFromSuperview]; 
+      [scroll removeFromSuperview]; 
       [self addSubview: errLabel];       
-			valid = NO;
+      valid = NO;
     }
-  }  
 }
 
 - (void)displayLastPath:(BOOL)forced
@@ -271,9 +275,9 @@
 
 - (BOOL)canDisplayPath:(NSString *)path
 {
-	NSString *defApp = nil, *fileType = nil;
+  NSString *defApp = nil, *fileType = nil;
 		
-	[ws getInfoForFile: path 
+  [ws getInfoForFile: path 
          application: &defApp 
                 type: &fileType];
 	
@@ -287,12 +291,12 @@
 
 - (NSString *)winname
 {
-	return NSLocalizedString(@"Application Inspector", @"");
+  return NSLocalizedString(@"Application Inspector", @"");
 }
 
 - (NSString *)description
 {
-	return NSLocalizedString(@"Displays info about an application bundle", @"");	
+  return NSLocalizedString(@"Displays info about an application bundle", @"");	
 }
 
 - (void)setContextHelp
